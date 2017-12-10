4d45125dd6cf330fbd51363f526b58c3ce66ab2b
soheil <azadvarsoheil@gmail.com>
2017-12-10 22:57:06 +0330

    app
    app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 logger = logging.getLogger(__name__)
-TOKEN = 'YOUR-TELEGRAM-BOT-TOKEN'
+TOKEN = '507036487:AAEFFjJwrgTnViuCAK6wGqCzRhLqC8a7Xwk'
 
 
 def start(bot, update):
